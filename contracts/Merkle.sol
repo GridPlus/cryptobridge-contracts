a8fb7f9b1d8c25d4933e7308afc3eb1ff9da6c76
Alex Miller <asmiller1989@gmail.com>
2017-12-30 14:51:03 -0800

    Adds a bunch of pseudocode logic to outline challenging header roots

--- contracts/Merkle.sol
+++ contracts/Merkle.sol
@@ -5,14 +5,19 @@ library Merkle {
 
   }
 
+  // Get the has of a header given the data that went into it
+  function getHeaderHash() public constant returns (bytes32) {
+    
+  }
+
   // Return the root of a Merkle tree. Two leaves are included, which must be
   // consecutive. Nodes are included, which are bytes32 hahses. The nodes must
   // be ordered such that the first is on the lowest branch, e.g:
   //
   //             X
   //       n2         |
-  //     |    |   n1    |
-  //    | |  | | | |  l1 l2
+  //     |    |   n1     |
+  //    | |  | | |  |  l1 l2
   //
   // In this example we provide l1 and l2 (ordered from right to left) and are
   // able to prove X. n1 (node 1) is in the lowest level, followed by n2.
@@ -20,8 +25,44 @@ library Merkle {
   //
   // Here, txIndex is 3 because it is in the fourth set of the lowest level.
   // (0-indexed) If the txs had been on the left side, txIndex would be 0.
-  function getTxRoot(bytes32[2] leaves, bytes[] nodes, uint256 i)
-    public constant returns (bytes32) {
+  function getRoot(bytes32[2] leaves, uint256 i, bytes nodes)
+  public constant returns (bytes32) {
+    // 1. Determine which side the leaves are on.
+    bytes32 current = sha3(leaves[0], leaves[1]);
 
+    // 2. Traverse the tree
+    for (uint256 j = 0; j < nodes.length / 32 - 1; j++) {
+      if (i % 2 == 0 || ) {
+        // node is on the left side
+        current = keccak256(nodes[j * 32 : j * 32 + 32], current);
+      } else {
+        // node is on the right side
+        current = keccak256(current, nodes[j * 32 : j * 32 + 32]);
+      }
+      // Divide i by 2 and round down to get the new index
+      i /= 2;
+    }
+    // 3. Return the root
+    return current;
   }
 }
+
+
+
+
+
+/*
+
+Figure for understanding getTxRoot()
+
+                 |                                      |
+
+
+        |        0          |                   |        1          |
+
+   |    0    |         |    1     |       |   2      |        |    3     |
+
+| 0  |    |  1  |   | 2   |    |  3 |  |  4  |   |  5  |   |  6  |    | 7   |
+
+
+*/
--- contracts/TrustlessRelay.sol
+++ contracts/TrustlessRelay.sol
@@ -1,5 +1,6 @@
 pragma solidity ^0.4.18;
 
+import "./Merkle.sol";
 import "tokens/Token.sol";  // truffle package (install with `truffle install tokens`)
 import "tokens/HumanStandardToken.sol";
 
@@ -9,7 +10,10 @@ contract TrustedRelay {
 
 
   address public admin;
-  uint256 public width = 1024; // Number of blocks included in a header root
+  // Reward for successfully contesting a headerRoot
+  uint256 public bountyWei = 1000000000000000;
+  // Number of blocks included in a headerRoot
+  uint256 public width = 1024;
   // The root of a Merkle tree made of consecutive block headers.
   // These are indexed by the address of the TrustlessRelay contract on the
   // origin chain. This also serves as the identity of the chain itself.
@@ -24,7 +28,32 @@ contract TrustedRelay {
     HeaderRoot(origin, root, headerRoots[origin].length, msg.sender);
   }
 
-  function 
+  // Anyone can contest a header that has been stored and can overwrite it if
+  // they can prove that it is incorrect.
+  // address origin       The address-id of the origin chain
+  // uint256 loc          The index of the headerRoot in question in headerRoots
+  // 
+  function contestHeader(address origin, uint256 loc, bytes32 txPartner, bytes32 headerPartner,
+  uint256 txI, uint headerI, bool txIsLeft, bool headerIsLeft,
+  bytes txData, bytes headerData, bytes txNodes, bytes headerNodes) public {
+    bytes32 txHash = Merkle.getTxHash(txData);
+    bytes32 txRoot;
+    if (txIsLeft == true) {
+      txRoot = Merkle.getRoot([txHash, txPartner], txI, txNodes);
+    } else {
+      txRoot = Merkle.getRoot([txPartner, txHash], txI, txNodes);
+    }
+    bytes32 headerHash = Merkle.getHeaderHash(headerData);
+    bytes32 headerRoot;
+    if (headerIsLeft == true) {
+      headerRoot = Merkle.getRoot([headerHash, headerPartner], headerI, headerNodes);
+    } else {
+      headerRoot = Merkle.getRoot([headerPartner, headerHash], headerI, headerNodes);
+    }
+    require(headerRoots[origin][loc] != headerRoot);
+    headerRoots[origin][loc] = headerRoot;
+    msg.sender.send(bountyWei);
+  }
 
 
   function TrustedRelay() {
